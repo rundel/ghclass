822468cd3a93077a25680d18b15673bc80351ca1
Colin Rundel <rundel@gmail.com>
2016-10-13 09:31:58 -0400

    Knit all the things

---
+++ knit_rmds.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+
+library(magrittr)
+library(knitr)
+library(rmarkdown)
+
+args = commandArgs(trailingOnly=TRUE)
+
+if(length(args) != 1)
+{
+  cat("Usage: knit_rmds.R <local repos dir>\n")
+  stop()
+}
+
+local_path = args[1]
+
+stopifnot(dir.exists(local_path))
+
+repos = list.dirs(path = local_path, full.names = TRUE, recursive = FALSE)      
+
+for(repo in repos)
+{
+  cat("Knitting in", repo, "...\n")
+
+  setwd(repo)
+  rmds = list.files("./", pattern="[Rr]md$")
+  lapply(rmds, render, quiet=TRUE)
+}
