9efbbd0789241d366de726e0e07de8abe303be45
Colin Rundel <rundel@gmail.com>
2016-12-04 23:58:25 -0500

    Update

--- grab_repos.R
+++ grab_repos.R
@@ -8,15 +8,19 @@ suppressMessages(library(git2r))
 args = commandArgs(trailingOnly=TRUE)
 token = readLines("secret/github_token")
 
-if(length(args) != 3)
+if (!length(args) %in% c(3,4))
 {
-  cat("Usage: grab_repos.R <organization> <repo pattern> <local path>\n")
+  cat("Usage: grab_repos.R <organization> <repo pattern> <local path> [progress=FALSE]\n")
   stop()
 }
 
 org = args[1]
 pattern = args[2]
 local_path = args[3]
+
+progress = FALSE
+if (length(args) == 4)
+  progress = as.logical(args[4])
 
 repos = gh("GET /orgs/:org/repos", org = org, .token=token, .limit=1000)
 repo_names = sapply(repos, function(x) x$name)   
@@ -30,8 +34,8 @@ for(repo in selected_repos)
   org_url = paste0('git@github.com:',org,'/',repo,'.git')
 
   d = file.path(local_path,repo)
-  dir.create(d, recursive=TRUE)
+  dir.create(d, recursive=TRUE, showWarnings=FALSE)
 
-  local_repo = clone(org_url, d, progress=FALSE)  
+  local_repo = clone(org_url, d, progress=progress)  
 }
 
