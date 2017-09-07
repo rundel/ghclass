ce06b9a75fb72860e6645b3d6857cc18ef0d6c82
Colin Rundel <rundel@gmail.com>
2017-09-06 22:34:15 -0400

    Fix typo

--- R/git.R
+++ R/git.R
@@ -5,7 +5,7 @@ require_git = function()
   if (git == "")
   {
     stop("git executable not found, if it is installed,",
-         "please make sure gitcan be found via the PATH variable.")
+         "please make sure git can be found via the PATH variable.")
   }
 
   return(git)
