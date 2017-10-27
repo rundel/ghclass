e1e968bf51a3e8ecbe60a2c47b096f665f31876d
Colin Rundel <rundel@gmail.com>
2017-10-27 11:44:33 -0400

    Tidy

--- R/git.R
+++ R/git.R
@@ -1,9 +1,8 @@
 require_git = function()
 {
-  git = Sys.which( "git" )
+  git = Sys.which("git")
 
-  if (git == "")
-  {
+  if (git == "") {
     stop("git executable not found, if it is installed,",
          "please make sure git can be found via the PATH variable.")
   }
