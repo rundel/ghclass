86134e1bae79843121e5334fdb205f238aa4b304
Colin Rundel <rundel@gmail.com>
2019-06-26 12:17:36 +0100

    Update core doc

--- R/ghclass.R
+++ R/ghclass.R
@@ -1,51 +1,90 @@
 #' ghclass: A package for github based classroom and assignment management
 #'
-#' @section github repo functions:
+#' @section GitHub repositoryy functions:
 #'
-#' \itemize{
-#'   \item \code{One} -
-#'   \item \code{Two} -
-#' }
+#' * [create_repo()] -
+#' * [add_team_to_repo()] -
+#' * [add_user_to_repo()] -
+#' * [mirror_repo()] -
+#' * [rename_repo()] -
 #'
-#' @section local repo functions:
+#' @section Local git repository functions:
 #'
-#' \itemize{
-#'   \item \code{\link{clone_repo}} - clone repos locally
-#'   \item \code{\link{pull_repo}} - update local repos via pull
-#' }
+#' * [clone_repo()] -
+#' * [add_repo()] -
+#' * [commit_repo()] -
+#' * [push_repo()] -
+#' * [pull_repo()] -
+#' * [rename_local_repo()] -
 #'
 #'
-#' @section github organization functions:
+#' @section Organization functions:
 #'
-#' \itemize{
-#'   \item \code{One} -
-#'   \item \code{Two} -
-#' }
+#' * [get_repo()] -
+#' * [get_admin()] -
+#' * [get_collaborator()] -
+#' * [get_member()] -
+#' * [get_pending_member()] -
+#' * [invite_user()] -
 #'
-#' @section wercker functions:
 #'
-#' \itemize{
-#'   \item \code{One} -
-#'   \item \code{Two} -
-#' }
+#' @section Team functions:
 #'
-#' @section authentication functions:
+#' * [get_teams()] -
+#' * [create_team()] -
+#' * [rename_team()] -
+#' * [add_team_member()] -
+#' * [get_team_members()] -
+#' * [get_pending_team_members()] -
+#' * [get_team_repos()] -
 #'
-#' GitHub
-#' \itemize{
-#'   \item \code{\link{get_github_token}} - get github token
-#'   \item \code{\link{set_github_token}} - set github token
-#'   \item \code{\link{test_github_token}} - test github token
-#' }
-#' Wercker
-#' \itemize{
-#'   \item \code{\link{get_wercker_token}} - get wercker token
-#'   \item \code{\link{set_wercker_token}} - set wercker token
-#'   \item \code{\link{test_wercker_token}} - test wercker token
-#' }
+#'
+#' @section GitHub authentication functions:
+#'
+#' * [get_github_token()] - get github token
+#' * [set_github_token()] - set github token
+#' * [test_github_token()] - test github token
+#'
+#'
+#' @section Repository file functions:
+#'
+#' * [add_file()] -
+#' * [add_content()] -
+#' * [get_file()] -
+#' * [get_readme()] -
+#' * [put_file()] -
+#'
+#'
+#' @section Branch functions:
+#'
+#' * [create_branch()] -
+#' * [create_pull_request()] -
+#' * [protect_branch()] -
+#' * [unprotect_branch()] -
+#'
+#'
+#' @section Notification functions:
+#'
+#' * [get_watching()] -
+#' * [ignore_repo()] -
+#' * [unwatch_repo()] -
+#' * [watch_repo()] -
+#'
+#'
+#' @section Utililty functions:
+#'
+#' * [check_repo()] -
+#' * [check_user_exists()] -
+#'
+#'
+#' @section Experimental functions:
+#'
+#' * [style_repo()] - use `styler` on all R code within a repo and create a branch + pull request with the result
+#'
 #'
 #' @import gh
 #'
 #' @docType package
 #' @name ghclass
 NULL
+
