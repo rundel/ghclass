70528a21fd22496f701591336f8bcbd37ec712b9
Colin Rundel <rundel@gmail.com>
2019-07-02 11:11:43 +0100

    Fix some docs and add team_delete

--- R/team.R
+++ R/team.R
@@ -5,17 +5,12 @@ github_api_get_teams = function(org) {
 
 #' Get organization teams
 #'
-#' \code{get_teams} returns a (filtered) data frame of teams in the organization with columns for
+#' `get_teams` returns a (filtered) data frame of teams in the organization with columns for
 #' their names (`name`) and their unique ids (`id`).
 #'
 #' @param org character, name of the GitHub organization.
 #' @param filter character, regex pattern for matching (or excluding) repos.
 #' @param exclude logical, should entries matching the regex be excluded or included.
-#'
-#' @examples
-#' \dontrun{
-#' get_team_repos("ghclass",c("team01","team02"))
-#' }
 #'
 #' @family github organization team related functions
 #'
@@ -262,6 +257,7 @@ github_api_create_team = function(org, name, privacy) {
 #' @family github organization team related functions
 #'
 #' @export
+#'
 create_team = function(org, team, privacy = c("closed","secret")) {
   team = unique(as.character(team))
   privacy = match.arg(privacy)
@@ -395,6 +391,59 @@ add_team_member = function(org, user, team, create_missing_teams = TRUE) {
         res,
         glue::glue("Added {usethis::ui_value(user)} to team {usethis::ui_value(team)}."),
         glue::glue("Failed to add {usethis::ui_value(user)} to team {usethis::ui_value(team)}.")
+      )
+    }
+  )
+}
+
+
+
+
+github_api_team_delete = function(team_id) {
+  gh::gh("DELETE /teams/:team_id",
+         team_id = team_id,
+         .token = get_github_token())
+}
+
+#' Delete team
+#'
+#' `team_delete` deletes an existing team from a GitHub organization.
+#'
+#' @param org Character. Name of the GitHub organization.
+#' @param team Character. Name of the GitHub team within that organization.
+#' @param prompt Logical. Should the user be prompted before deleting repositories. Default `true`.
+#'
+#' @family github organization team related functions
+#'
+#' @export
+#'
+team_delete = function(org, team, prompt = TRUE) {
+
+  arg_is_chr_scalar(org)
+  arg_is_chr(team)
+  arg_is_lgl_scalar(prompt)
+
+  if (prompt) {
+    delete = usethis::ui_yeah( paste(
+      "This command will delete the following teams permanently:",
+      "{usethis::ui_value(team)}."
+    ) )
+    if (!delete) {
+      return(invisible())
+    }
+  }
+
+  team = get_specific_teams(org, team)
+
+  purrr::pwalk(
+    team,
+    function(team, id) {
+      res = purrr::safely(github_api_team_delete)(id)
+
+      status_msg(
+        res,
+        glue::glue("Deleted team {usethis::ui_value(team)} from org {usethis::ui_value(org)}."),
+        glue::glue("Failed to delete team {usethis::ui_value(team)} from org {usethis::ui_value(org)}.")
       )
     }
   )
