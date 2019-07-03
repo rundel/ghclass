c99306ee06f418fc5217e78cb5d6f1b7741ab771
Colin Rundel <rundel@gmail.com>
2019-07-03 13:31:03 +0100

    Rename test token

--- R/github_test_token.R
+++ R/github_test_token.R
@@ -1,21 +1,21 @@
 #' Test github token
 #'
-#' test_github_token` checks if a token is valid by attempting to authenticate
+#' github_test_token` checks if a token is valid by attempting to authenticate
 #' with the GitHub api.
 #'
 #' @param token github api personal access token
 #'
 #' @examples
 #' \dontrun{
-#' test_github_token()
-#' test_github_token("bad_token")
+#' github_test_token()
+#' github_test_token("bad_token")
 #' }
 #'
 #' @family authentication
 #'
 #' @export
 
-test_github_token = function(token = get_github_token()) {
+github_test_token = function(token = get_github_token()) {
   res = purrr::safely(gh::gh)("/user", .token=token)
 
   status_msg(
