0d8bcd421645231662c62ff3f304c6121fe91047
Therese Anders <26805373+thereseanders@users.noreply.github.com>
2019-07-12 13:24:47 -0700

    Adding folder functionality to repo_add_file

--- R/repo_add_file.R
+++ R/repo_add_file.R
@@ -12,8 +12,9 @@ read_bin_file = function(x) {
 #' `repo_add_file` uses the GitHub API to add/update files in an existing repo on GitHub. Note that due to time delays in caching, files that have been added very recently might not yet be displayed as existing and might accidentally be overwritten.
 #'
 #' @param repo Character. Address of repository in "owner/name" format.
-#' @param message Character. Commit message. If not provided, a custom character string will be created, in the form of "Added file(s): filename(s)". If this custom message character length exceeds 50, it will be shortened to "Added file(s)".
 #' @param file Character. Local file path(s) of file or files to be added.
+#' @param message Character. Commit message. If not provided, a custom character string will be created, in the form of "Added file(s): filename(s)". If this custom message character length exceeds 50, it will be shortened to "Added file(s)".
+#' @param folder Character. Name of folder on repository to save the file(s) to. Will be newly created if it does not  exist.
 #' @param branch Character. Name of branch to use, defaults to "master".
 #' @param preserve_path Logical. Should the local relative path be preserved.
 #' @param overwrite Logical. Should existing file or files with same name be overwritten, defaults to FALSE.
@@ -28,11 +29,11 @@ read_bin_file = function(x) {
 #'
 #' @export
 #'
-repo_add_file = function(repo, file, message = NULL, branch = "master",
+repo_add_file = function(repo, file, message = NULL, folder = NULL, branch = "master",
                     preserve_path = FALSE, overwrite = FALSE) {
 
   arg_is_chr(repo, file, branch)
-  arg_is_chr(message, allow_null = TRUE)
+  arg_is_chr(folder, message, allow_null = TRUE)
   arg_is_lgl_scalar(preserve_path, overwrite)
 
   file_status = fs::file_exists(file)
@@ -52,8 +53,11 @@ repo_add_file = function(repo, file, message = NULL, branch = "master",
         file,
         function(file){
           gh_path = file
-          if(!preserve_path)
+          if (!preserve_path)
             gh_path = fs::path_file(file)
+          if(!is.null(folder))
+            gh_path = sub(fs::path_file(file), paste(folder, fs::path_file(file), sep = "/"), gh_path)
+
 
           if(!check_file_modification(repo, gh_path, branch) | overwrite){
             repo_put_file(
