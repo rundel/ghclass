78a6e1e8939fa582aa3d48f1c864300f6cc98288
Therese Anders <26805373+thereseanders@users.noreply.github.com>
2019-07-18 16:54:17 -0700

    Add functionality to create labels and issues

---
+++ R/util_peer.R
@@ -0,0 +1,51 @@
+github_api_issue_create = function(repo, title, body, assignee, label) {
+  gh::gh(
+    "POST /repos/:owner/:repo/issues",
+    owner = get_repo_owner(repo),
+    repo = get_repo_name(repo),
+    title = title,
+    body = body,
+    assignee = assignee,
+    label = label,
+    .token = github_get_token()
+  )
+}
+
+github_api_label_create = function(repo, name, color, description) {
+  gh::gh(
+    "POST /repos/:owner/:repo/labels",
+    owner = get_repo_owner(repo),
+    repo = get_repo_name(repo),
+    name = name,
+    color = color,
+    description = description,
+    .token = github_get_token()
+  )
+}
+
+peer_create_label = function(repo, verbose = FALSE) {
+
+  arg_is_chr(repo)
+
+  name = c(":pencil: Give feedback", ":mag: View feedback")
+  color = c("ff004d", "ffb400")
+  desc = c("Please provide feedback", "Please view and rate feedback")
+
+  purrr::pwalk(list(name, color, desc),
+               function(name, color, desc) {
+                 res = purrr::safely(github_api_label_create)(
+                   repo = repo,
+                   name = name,
+                   color = color,
+                   description = desc
+                 )
+
+                 if (verbose) {
+                   status_msg(
+                     res,
+                     glue::glue("Created label {usethis::ui_value(name)}"),
+                     glue::glue("Failed to create label {usethis::ui_value(name)}")
+                   )
+                 }
+               })
+}
