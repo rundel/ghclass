cbca167ec34a3fb84b3100fd36dc8992e6d51c79
Colin Rundel <rundel@gmail.com>
2019-07-19 21:22:50 +0100

    Function to retrieve files df from a repo

--- R/util_github.R
+++ R/util_github.R
@@ -8,6 +8,24 @@ github_api_repo_get_tree = function(repo, sha = "master") {
   )
 }
 
+repo_files = function(repo, branch = "master") {
+  purrr::map2_dfr(
+    repo, branch,
+    function(repo, branch) {
+      print("here")
+      res = purrr::safely(github_api_repo_get_tree)(repo, branch)
+
+      if (failed(res)) {
+        r = usethis::ui_value(format_repo(repo, branch))
+        usethis::ui_oops("Failed to retrieve files for repo {r}")
+        return(NULL)
+      }
+
+      purrr::map_dfr(result(res)[["tree"]], ~c(repo = repo, .))
+    }
+  )
+}
+
 github_api_org_accept_invite = function(org, token) {
   arg_is_chr_scalar(org, token)
 
