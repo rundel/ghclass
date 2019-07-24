bfd695edfd27f1bff271db10507efa10eefe9381
Therese Anders <26805373+thereseanders@users.noreply.github.com>
2019-07-24 14:24:28 -0700

    Repo add file enhancement (#53)
    * Adding folder functionality to repo_add_file
    
    * Minor documentation change
    
    * Taking out unnecessary parameters
    
    * Removed turning message into list (causes repo_put_file to fail when no commit message is provided).
    
    * Fixing example to mirror sequence of parameters in function
    
    * Updating documentation
    
    * Fix bug - check_file_modification returns commits on deleted files and causes re-adding of files to fail.
    
    * Change file_exists to file_exists_ever to check whether file ever existing, adding new file_exist that does not use costly search API
    
    * Changing function name to current
    
    * Adding new check_file_everexist() function, adding modification check to repo_add_file(), simplifying file_exist().
    
    * Changing file_exist() to use new github_api_repo_get_tree()
    
    * Taking out now obsolete `verbose = F` from `file_exists` and enhancing error message of file_status
    
    * Small fix, iterate over branch but force message and folder to be a single value.
    
    * Changing folder to repo_folder

--- R/repo_add_file.R
+++ R/repo_add_file.R
@@ -7,20 +7,22 @@ read_bin_file = function(x) {
   raw
 }
 
+
 #' Add files to a repo
 #'
 #' `repo_add_file` uses the GitHub API to add/update files in an existing repo on GitHub. Note that due to time delays in caching, files that have been added very recently might not yet be displayed as existing and might accidentally be overwritten.
 #'
 #' @param repo Character. Address of repository in "owner/name" format.
-#' @param message Character. Commit message. If not provided, a custom character string will be created, in the form of "Added file(s): filename(s)". If this custom message character length exceeds 50, it will be shortened to "Added file(s)".
 #' @param file Character. Local file path(s) of file or files to be added.
+#' @param message Character. Commit message. If not provided, a custom character string will be created, in the form of "Added file(s): filename(s)". If this custom message character length exceeds 50, it will be shortened to "Added file(s)".
+#' @param repo_folder Character. Name of folder on repository to save the file(s) to. If the folder does not exist on the repository, it will be created.
 #' @param branch Character. Name of branch to use, defaults to "master".
 #' @param preserve_path Logical. Should the local relative path be preserved.
 #' @param overwrite Logical. Should existing file or files with same name be overwritten, defaults to FALSE.
 #'
 #' @examples
 #' \dontrun{
-#' repo_add_file("rundel/ghclass", "Update DESCRIPTION", "./DESCRIPTION")
+#' repo_add_file("rundel/ghclass", "./DESCRIPTION", "Update DESCRIPTION")
 #' }
 #'
 
@@ -28,34 +30,35 @@ read_bin_file = function(x) {
 #'
 #' @export
 #'
-repo_add_file = function(repo, file, message = NULL, branch = "master",
-                    preserve_path = FALSE, overwrite = FALSE) {
+repo_add_file = function(repo, file, message = NULL, repo_folder = NULL, branch = "master",
+                         preserve_path = FALSE, overwrite = FALSE) {
 
   arg_is_chr(repo, file, branch)
-  arg_is_chr(message, allow_null = TRUE)
+  arg_is_chr_scalar(repo_folder, message, allow_null = TRUE)
   arg_is_lgl_scalar(preserve_path, overwrite)
 
   file_status = fs::file_exists(file)
   if (any(!file_status))
-    usethis::ui_stop("Unable to locate the following file(s): {usethis::ui_value(file)}")
-
-  if (is.null(message))
-    message = list(NULL)
+    usethis::ui_stop("Unable to locate the following file(s): {usethis::ui_value(file[!file_status])}")
 
   if (is.character(file) & (length(file) > 1))
     file = list(file)
 
   purrr::pwalk(
-    list(repo, file, message, branch),
-    function(repo, file, message, branch){
+    list(repo, file, branch),
+    function(repo, file, branch) {
       purrr::walk(
         file,
         function(file){
           gh_path = file
-          if(!preserve_path)
+
+          if (!preserve_path)
             gh_path = fs::path_file(file)
 
-          if(!check_file_modification(repo, gh_path, branch) | overwrite){
+          if(!is.null(repo_folder))
+            gh_path = fs::path(repo_folder, gh_path)
+
+          if (!file_exists(repo, gh_path, branch) | overwrite) {
             repo_put_file(
               repo = repo,
               path = gh_path,
@@ -65,9 +68,13 @@ repo_add_file = function(repo, file, message = NULL, branch = "master",
               verbose = TRUE
             )
           } else {
+
             usethis::ui_oops( paste(
-              'Failed to add file {usethis::ui_value(gh_path)} to repo {usethis::ui_value(repo)}, file already exists.',
-              'If you want to force add this file, re-run the command with {usethis::ui_code("overwrite = TRUE")}.'
+              'Failed to add file {usethis::ui_value(gh_path)} to repo {usethis::ui_value(repo)}:  already exists.\n',
+              if (check_file_modification(repo, gh_path, branch)) {
+                'File has been modified after initial commit.\n'
+              },
+              'If you want to force-add this file, re-run the command with {usethis::ui_code("overwrite = TRUE")}.'
             ) )
           }
         })
