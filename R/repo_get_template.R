6df90a2f2e2774746ce45113a2b4e125168b2813
Colin Rundel <rundel@gmail.com>
2019-09-03 16:01:11 +0100

    New functions for checking and modifying repos wrt templates

---
+++ R/repo_get_template.R
@@ -0,0 +1,37 @@
+#' Check if a GitHub repository is a template repository
+#'
+#' `repo_get_template` returns TRUE if the github repository is a template repository.
+#'
+#' @param repo Character. Address of repository in "owner/name" format.
+#'
+#' @examples
+#' \dontrun{
+#' repo_get_template(c("rundel/ghclass", "rundel/ghclass_fake"))
+#' }
+#'
+#' @return A logical vector
+#'
+#' @export
+#'
+repo_get_template = function(repo) {
+
+  arg_is_chr(repo)
+
+  # Checking if repo exists
+  purrr::map_lgl(
+    repo,
+    function(repo) {
+      res = purrr::safely(github_api_repo)(repo)
+      status_msg(
+        res,
+        fail = glue::glue("Failed to retrieve repo {usethis::ui_value(repo)}.")
+      )
+
+      if (succeeded(res)) {
+        result(res)[["is_template"]]
+      } else {
+        NA
+      }
+    }
+  )
+}
