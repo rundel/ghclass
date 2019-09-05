58f21cfe2a9a11038f23e513692b0fc62d190a76
Therese Anders <26805373+thereseanders@users.noreply.github.com>
2019-09-05 14:56:34 -0700

    Peer 190802 (#66)
    * Adding peer review related files
    
    * reflecting upstream master changes and fixing smaller issues
    
    * check for peer_create_aform and vignette enhancements
    
    * Simplifying getting content (eliminate unnecessary calls to file_exists()
    
    * Add functionality to create labels and issues
    
    * Adding path parameter to get committer
    
    * add max points feature, change to aut and rev, change feedback form name
    
    * Adding repo_mirror_file
    
    * Creating helper functions and more encompassing version of peer_return()
    
    * Creating helper functions and more encompassing version of peer_return()
    
    * Fixing repo_add_file
    
    * Minor naming convention fixes
    
    * Adding functionality to move files via trees API
    
    * Changing a range of functions (separating, streamlining)
    
    * Adding local repo version of moving files
    
    * Enhance functions; fix issue creation within peer_return(), add local repo version peer_assign_clone()
    
    * Cleaning up
    
    * improving peer_assign, reducing calls to API
    
    * Addind review issue creation
    
    * Moving repo_mirror_file to separate file, fixing a few functions
    
    * Fixing bugs
    
    * Fixing bugs and typos
    
    * Updating documentation
    
    * Implementing bug fixes
    
    * reducing api calls
    
    * updating vignette
    
    * Update vignette
    
    * Update vignette
    
    * Updating documentation
    
    * Fixing repo_mirror_file
    
    * Moving files to peer_util.R to clean up
    
    * Helper function for peer_add_file_* functions
    
    * Bug fix
    
    * Adding .Rproj to list of files to be excluded from peer_assign()
    
    * Instructions for students enhancements
    
    * Reorder YAML to match other vignettes, and update title
    
    * Rebuilding vignettes
    
    * enhancement in vignette
    
    * initial changes for PR #66
    
    * adding vignette (built remotely)
    
    * Fixing bug in writing to file
    
    * deleting html vignette
    
    * Fixing instructions to students
    
    * Adding html for peer vignetten
    
    * Don't need load_all()
    
    * Bug fixes and documentation changes
    
    * Adding templates, improving peer_return
    
    * peer_assign() enhancements, adding arg_is_raw
    
    * Enhance label application
    
    * Changing vignette, fixing peer_score_review and peer_score_rating
    
    * Improving peer_return (reducing API calls)
    
    * Fixing bugs in peer_return and peer_assign. updating documentation
    
    * Bug fixes that prevented peer_return() from running
    
    * Minor changes in vignette and cleaning up
    
    * Moving experimental functions into separate branch
    
    * Removing stringr dependency and updating pkgdown
    
    * Update gitignore
    
    * Updating vignette peer_return up to 294 students
    
    * Implementing requested changes for PR #66.
    
    * Additional changes for PR #66
    
    * Updating documentation and peer review vignette
    
    * Fixing CMD check notes and updating imports in DESCRIPTION
    
    * Minor change to vignette and to peer_roster_process()

--- R/repo_put_file.R
+++ R/repo_put_file.R
@@ -54,3 +54,54 @@ repo_put_file = function(repo, path, content, message = NULL, branch = "master",
 
   res
 }
+
+
+
+
+peer_github_api_repo_put_file = function(repo, path, content, message, branch, sha) {
+
+  args = list(
+    endpoint = "PUT /repos/:owner/:repo/contents/:path",
+    owner = get_repo_owner(repo), repo = get_repo_name(repo),
+    path = path,
+    content = base64enc::base64encode(content),
+    message = message, branch = branch,
+    .token = github_get_token()
+  )
+
+  if (!is.null(sha)) {
+    args[["sha"]] = sha
+  }
+
+  do.call(gh::gh, args)
+}
+
+
+
+peer_repo_put_file = function(repo, path, content, message = NULL, branch = "master", sha, verbose = TRUE) {
+  arg_is_chr_scalar(repo, path, branch)
+  arg_is_chr_scalar(message, sha, allow_null = TRUE)
+
+  if (is.null(message))
+    message = glue::glue("Adding file: {path}")
+
+  if (is.character(content))
+    content = charToRaw(content)
+
+  res = purrr::safely(peer_github_api_repo_put_file)(repo = repo,
+                                                     path = path,
+                                                     content = content,
+                                                     message = message,
+                                                     branch = branch,
+                                                     sha = sha)
+
+  if(verbose){
+    status_msg(
+      res,
+      glue::glue("Added file {usethis::ui_value(path)} to repo {usethis::ui_value(repo)}."),
+      glue::glue("Failed to add file {usethis::ui_value(path)} to repo {usethis::ui_value(repo)}.")
+    )
+  }
+
+  res
+}
