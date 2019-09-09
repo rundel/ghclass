c38d3462e1166ed3fc2ff56b7437b509ced3c00e
Colin Rundel <rundel@gmail.com>
2019-09-09 15:23:19 +0100

    Roxygen aliases changed or I screwed it up, this fixes some R CMD check stuff

--- R/repo_get_file.R
+++ R/repo_get_file.R
@@ -17,7 +17,7 @@ github_api_repo_get_file = function(repo, path, branch) {
 #' Low level function for retrieving a file from a GitHub Repository
 #'
 #' @param repo Character. Address of repository in `owner/name` format.
-#' @param file Characer. Path to the file within the repository.
+#' @param path Characer. Path to the file within the repository.
 #' @param branch Character. Name of branch to use, defaults to "master".
 #'
 
