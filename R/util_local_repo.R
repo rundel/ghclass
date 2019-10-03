95ad2db1c6a7c859d5a558c26390cc7c58daf321
Colin Rundel <rundel@gmail.com>
2019-10-03 14:54:23 +0100

    Fix mirror to use gert based functions

--- R/util_local_repo.R
+++ R/util_local_repo.R
@@ -1,14 +1,21 @@
+is_git_repo = function(path) {
+  arg_is_chr_scalar(path)
+
+  res = purrr::safely(gert::git_open)(path)
+  succeeded(res)
+}
+
 # If we are given a single repo directory check if it is a repo or a directory of repos
 repo_dir_helper = function(repo_dir) {
 
   exists = fs::dir_exists(repo_dir)
   if(!all(exists)) {
     usethis::ui_stop( paste(
-      "Unable to locate the repos: {usethis::ui_value(repo_dir[!exists])}."
+      "Unable to locate the repo(s): {usethis::ui_value(repo_dir[!exists])}."
     ) )
   }
 
-  if (length(repo_dir) == 1 & !fs::dir_exists(fs::path(repo_dir[1],".git"))) {
+  if (length(repo_dir) == 1 & !is_git_repo(repo_dir[1])) {
     dir = fs::dir_ls(repo_dir, type="directory")
   } else {
     dir = repo_dir
