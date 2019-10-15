cfcbc55814814b2618c684e35f2bcd8dc049a39f
Colin Rundel <rundel@gmail.com>
2019-10-15 14:47:45 +0100

    Remove processx / git stuff

--- R/util_local_repo.R
+++ R/util_local_repo.R
@@ -23,26 +23,3 @@ repo_dir_helper = function(repo_dir) {
 
   fs::path_real(dir)
 }
-
-censor_token = function(msg, replacement = "", prefix="", suffix="") {
-  pattern = paste0(prefix, github_get_token(), suffix)
-  sub(pattern, replacement, msg)
-}
-
-run_git = function(git = require_git(), cmd, args = character(), verbose=FALSE) {
-  stopifnot(!missing(cmd))
-
-  res = processx::run(
-    git, args  = c(cmd, args), error_on_status = FALSE, echo = verbose, echo_cmd = verbose
-  )
-
-  err_msg = res[["stderr"]]
-  err_msg = sub("fatal: ", "", err_msg)
-  err_msg = sub("^\\s|\\s$", "", err_msg)
-  err_msg = censor_token(err_msg, suffix="@")
-
-  if (res[["status"]] != 0)
-    stop(err_msg)
-
-  res
-}
