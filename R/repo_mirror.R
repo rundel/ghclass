af6b44968dab0947d6fa71565132aa50e174f6c5
Colin Rundel <rundel@gmail.com>
2019-10-18 12:12:30 +0100

    This should address the endless prompting with mirror_repo

--- R/repo_mirror.R
+++ R/repo_mirror.R
@@ -5,6 +5,7 @@
 #'
 #' @param source_repo Character. Address of repository in "owner/name" format.
 #' @param target_repo Character. One or more repository addresses in "owner/name" format.
+#' @param overwrite Logical. Should the target repositories be overwritten.
 #' @param verbose Logical. Display verbose output.
 #'
 #' @examples
@@ -17,14 +18,34 @@
 #'
 #' @export
 #'
-repo_mirror = function(source_repo, target_repo, verbose=FALSE) {
+repo_mirror = function(source_repo, target_repo, overwrite=FALSE, verbose=FALSE) {
   arg_is_chr_scalar(source_repo)
   arg_is_chr(target_repo)
+  arg_is_lgl_scalar(overwrite, verbose)
 
   withr::local_dir(tempdir())
 
   dir = file.path(getwd(), get_repo_name(source_repo))
   unlink(dir, recursive = TRUE)
+
+  n_commits = table(repo_commits(target_repo)[["repo"]])
+
+  missing_repos = target_repo[!target_repo %in% names(n_commits)]
+  if (length(missing_repos) != 0) {
+    usethis::ui_stop( paste(
+      "The following {usethis::ui_code('target_repo')}s do not exist:",
+      "{usethis::ui_value(missing_repos)}"
+    ) )
+  }
+
+  if (!overwrite & any(n_commits > 1)) {
+    usethis::ui_stop( paste(
+      "The following {usethis::ui_code('target_repo')}s have more than a single initialization commit:",
+      "\n\t{usethis::ui_value(target_repo[n_commits > 1])}.\n",
+      "This process will permanently overwrite these repositories. If you are sure this is what you want",
+      "to do re-run this function with {usethis::ui_code('overwrite = TRUE')}"
+    ) )
+  }
 
   local_repo_clone(source_repo, getwd(), mirror = TRUE, verbose = verbose)
 
@@ -32,7 +53,9 @@ repo_mirror = function(source_repo, target_repo, verbose=FALSE) {
     target_repo,
     function(repo) {
       repo_url = glue::glue("https://github.com/{repo}.git")
-      local_repo_push(dir, remote = repo_url, mirror = TRUE, force = TRUE, verbose = verbose)
+
+
+      local_repo_push(dir, remote = repo_url, force = TRUE, prompt = FALSE, mirror = TRUE, verbose = verbose)
     }
   )
 
