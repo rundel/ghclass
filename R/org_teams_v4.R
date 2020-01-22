cbb9014682dfcfe1168d6f2fb041c02feaa74cf4
Colin Rundel <rundel@gmail.com>
2020-01-22 21:33:42 +0000

    Add basic repo stats functions

---
+++ R/org_teams_v4.R
@@ -0,0 +1,56 @@
+github_api_org_teams_v4 = function(org) {
+  query = '
+    query {
+      organization(login: "{{org}}") {
+        teams(first: 100, after: <graphql_quote(cursor)>) {
+          pageInfo {
+             hasNextPage
+             endCursor
+          }
+          edges {
+            node {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  '
+
+  query = whisker::whisker.render(query)
+
+  github_api_v4_graphql_paginated(query, page_info = c("organization", "teams"))
+}
+
+
+
+#' Get organization teams
+#'
+#' @param org Character. Name of the GitHub organization.
+#'
+#' @examples
+#' \dontrun{
+#' org_teams_v4("ghclass-demo")
+#' }
+#'
+#' @export
+#'
+org_teams_v4 = function(org) {
+  arg_is_chr_scalar(org)
+
+  pages = github_api_org_teams_v4(org)
+
+  purrr::map_dfr(
+    pages,
+    function(page) {
+      teams = purrr::pluck(page, "data", "organization", "teams", "edges")
+
+      tibble::tibble(
+        org  = org,
+        team = purrr::map_chr(teams, c("node", "name")),
+        id   = purrr::map_chr(teams, c("node", "id"))
+      )
+    }
+  )
+}
---
+++ R/util_github_v4.R
@@ -0,0 +1,79 @@
+github_api_v4_graphql = function(query, vars) {
+  arg_is_chr_scalar(query)
+
+  query = graphql_glue(query, vars)
+
+  req = httr::POST(
+    "https://api.github.com/graphql",
+    httr::add_headers(
+      Authorization = paste("bearer", github_get_token())
+    ),
+    encode = "json",
+    body = list(query = query)
+  )
+
+  res = httr::content(req)
+  code = httr::status_code(req)
+
+  if (code >= 300) {
+    usethis::ui_stop("Github API v4 error code ({code}) - {res[['message']]}")
+  }
+
+  res
+}
+
+github_api_v4_graphql_paginated = function(query, page_info, cursor_var = "cursor") {
+  arg_is_chr_scalar(query)
+  arg_is_chr(page_info)
+
+  vars = list()
+  vars[cursor_var] = list(NULL)
+
+  page_info = as.list(page_info)
+  if (page_info[[1]] != "data")
+    page_info = c("data",page_info)
+  if (page_info[[length(page_info)]] != "pageInfo")
+    page_info = c(page_info, "pageInfo")
+
+
+  res = list()
+  i = 1
+
+  repeat {
+    res[[i]] = github_api_v4_graphql(query, vars)
+    page = purrr::pluck(res[[i]], !!!page_info)
+
+    if (!is.null(res[[i]]$errors)) {
+      msgs = purrr::map(res[[i]]$errors, "message")
+      msg = paste(unlist(msgs), collapse="\n")
+      usethis::ui_stop(msg)
+    }
+
+    if (is.null(page)) {
+      usethis::ui_stop("Unable to locate page info for this query.")
+    }
+
+    if (!page$hasNextPage)
+      break
+
+    vars[[cursor_var]] = page$endCursor
+    i = i + 1
+  }
+
+  res
+}
+
+
+graphql_glue = function(query, vars) {
+  glue::glue_data(vars, query, .open = "<", .close = ">")
+}
+
+graphql_quote = function(x) {
+  if (is.null(x)) {
+    "null"
+  } else if (is.na(x)) {
+    usethis::ui_stop("{usethis::ui_value(NA)} values are not supported by graphql.")
+  } else {
+    glue::double_quote(x)
+  }
+}
