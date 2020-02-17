7f194ec9976de35508794d1ef1ec671d289570ec
Colin Rundel <rundel@gmail.com>
2020-02-17 11:23:33 +0000

    Add repo_remove_user

---
+++ R/repo_remove_user.R
@@ -0,0 +1,36 @@
+github_api_repo_remove_user = function(repo, username, permission){
+  gh::gh("DELETE /repos/:owner/:repo/collaborators/:username",
+         owner = get_repo_owner(repo),
+         repo = get_repo_name(repo),
+         username = username,
+         permission = permission,
+         .token = github_get_token())
+}
+
+
+#' Remove a user from a repository
+#'
+#' @param repo Character. Address of repository in "owner/name" format.
+#' @param user Character. One or more GitHub usernames.
+#'
+#' @export
+repo_remove_user = function(repo, user) {
+  arg_is_chr(repo, user)
+
+  purrr::walk2(
+    repo, user,
+    function(repo, user) {
+      res = purrr::safely(github_api_repo_remove_user)(
+        repo = repo,
+        username = user,
+        permission = permission
+      )
+
+      status_msg(
+        res,
+        glue::glue("Removed user {usethis::ui_value(user)} from repo {usethis::ui_value(repo)}."),
+        glue::glue("Failed to remove user {usethis::ui_value(user)} from repo {usethis::ui_value(repo)}.")
+      )
+    }
+  )
+}
