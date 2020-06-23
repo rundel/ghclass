962fb9f59ed2c5f29a5c0b1aa958eac8ac073505
Colin Rundel <rundel@gmail.com>
2020-06-23 17:03:02 +0100

    gert::git_pull doesnt support remotes currently, so remove

--- R/local_repo_pull.R
+++ R/local_repo_pull.R
@@ -1,27 +1,30 @@
 #' @rdname local_repo
 #' @export
-local_repo_pull = function(repo_dir, remote="origin", branch="master", verbose = FALSE) {
+#'
+
+# TODO - add support for remotes when added to gert
+local_repo_pull = function(repo_dir, branch="master", verbose = FALSE) {
   require_gert()
 
-  arg_is_chr(repo_dir, remote, branch)
+  arg_is_chr(repo_dir, branch)
   arg_is_lgl_scalar(verbose)
 
   dir = repo_dir_helper(repo_dir)
 
   res = purrr::pmap(
-    list(dir, remote, branch),
-    function(dir, remote, branch) {
+    list(dir, branch),
+    function(dir, branch) {
       withr::local_dir(dir)
 
       res = purrr::safely(gert::git_pull)(
-        remote = remote, repo = dir, verbose = verbose
+        repo = dir, verbose = verbose
       )
 
       repo = fs::path_file(dir)
       status_msg(
         res,
-        "Pulled to {.val {repo}}.",
-        "Failed to pull to {.val {repo}}."
+        "Pulled to local repo {.val {repo}}.",
+        "Failed to pull to local repo {.val {repo}}."
       )
 
       res
