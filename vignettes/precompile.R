9b4c82a9d242fb6d1ee0e30fbcc84f17403026cf
Colin Rundel <rundel@gmail.com>
2020-07-13 12:00:13 +0100

    Update pkgdown

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -2,8 +2,8 @@
 
 # Vignettes that depend on GitHub PAT are precompiled:
 
-knitr::knit("vignettes/raw/ghclass.Rmd.orig", "vignettes/ghclass.Rmd")
-knitr::knit("vignettes/raw/instructions_students.Rmd.orig", "vignettes/instructions_students.Rmd")
-knitr::knit("vignettes/raw/peer.Rmd.orig", "vignettes/peer.Rmd")
+knitr::knit("vignettes/.raw/ghclass.Rmd", "vignettes/ghclass.Rmd")
+knitr::knit("vignettes/.raw/instructions_students.Rmd.orig", "vignettes/instructions_students.Rmd")
+knitr::knit("vignettes/.raw/peer.Rmd.orig", "vignettes/peer.Rmd")
 
 devtools::build_vignettes()
