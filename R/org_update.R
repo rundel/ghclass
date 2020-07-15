6b05f07dda83a8c27e404ae1bcfa7710646f0789
Colin Rundel <rundel@gmail.com>
2020-07-15 23:38:34 +0100

    Print org name not "org"

--- R/org_update.R
+++ R/org_update.R
@@ -28,8 +28,8 @@ org_set_repo_permission = function(org, permission = c("none", "read", "write", 
 
   status_msg(
     res,
-    "Set org {.val org}'s repo permissions to {.val permission}.",
-    "failed to set org {.val org}'s repo permissions."
+    "Set org {.val {org}}'s repo permissions to {.val permission}.",
+    "failed to set org {.val {org}}'s repo permissions."
   )
 
   invisible(result(res))
