be2c6295f19dcc11206ccddac3cc06e191d8f29c
Colin Rundel <rundel@gmail.com>
2020-07-16 00:09:15 +0100

    update articles

--- vignettes/articles/precompile.R
+++ vignettes/articles/precompile.R
@@ -3,15 +3,13 @@
 # Vignettes that depend on GitHub PAT are precompiled:
 
 withr::with_dir(
-  here::here("vignettes/"),
+  here::here("vignettes/articles"),
   {
-    print(getwd())
     knitr::knit("ghclass.Rmd_raw", "ghclass.Rmd")
     knitr::knit("peer_students.Rmd_raw", "peer_students.Rmd")
     #knitr::knit("peer.Rmd_raw", "peer.Rmd")
   }
 )
 
-
-
 devtools::build_vignettes()
+
