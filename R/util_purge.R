a264ca90140632538f9d8900962953f941cdcfc1
Colin Rundel <rundel@gmail.com>
2020-07-17 10:38:15 +0100

    Check lengths and purge pending

--- R/util_purge.R
+++ R/util_purge.R
@@ -1,9 +1,14 @@
 purge_all = function(org) {
   repos = org_repos(org)
   teams = org_teams(org)
-  users = org_members(org, include_admins = FALSE)
+  users = c( org_members(org, include_admins = FALSE), org_pending(org))
 
-  repo_delete(repos, prompt = TRUE)
-  team_delete(org, teams, prompt = TRUE)
-  org_remove(org, users, prompt = TRUE)
+  if (length(repos) != 0)
+    repo_delete(repos, prompt = TRUE)
+
+  if (length(teams) != 0)
+    team_delete(org, teams, prompt = TRUE)
+
+  if (length(users) != 0)
+    org_remove(org, users, prompt = TRUE)
 }
