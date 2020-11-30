1e3f58375266c14b024d80ac84c8e9836591fe54
Colin Rundel <rundel@gmail.com>
2020-11-30 13:24:32 +0000

    clean up implementation and include weekly stats since we're getting them anyway.

--- R/repo_contributors.R
+++ R/repo_contributors.R
@@ -1,9 +1,31 @@
-github_api_repo_contributors = function(repo) {
-  ghclass_api_v3_req(
-    endpoint = "GET /repos/:owner/:repo/stats/contributors",
-    owner = get_repo_owner(repo),
-    repo = get_repo_name(repo)
-  )
+github_api_repo_contributors = function(repo, timeout = 10, wait = 0.5) {
+  owner = get_repo_owner(repo)
+  repo = get_repo_name(repo)
+
+  make_req = function() {
+    ghclass_api_v3_req(
+      endpoint = "GET /repos/:owner/:repo/stats/contributors",
+      owner = owner,
+      repo = repo
+    )
+  }
+
+  start = Sys.time()
+
+  # Calculating these stats can take some time, initial request will begin the calculation
+  # and return a 202, we wait n seconds and try again. Timeout after m seconds.
+
+  while(difftime(Sys.time(), start, units = "secs") < timeout) {
+    r = make_req()
+
+    if (response_status(r) == "200 OK") {
+      return(r)
+    }
+
+    Sys.sleep(wait)
+  }
+
+  cli_stop("Timeout - no response within {timeout} seconds.")
 }
 
 #' @rdname repo_user
@@ -17,25 +39,7 @@ repo_contributors = function(repo) {
   purrr::map_dfr(
     repo,
     function(repo) {
-      # Calculating these stats can take some time, initial request will begin the calculation
-      # and return a 202, we wait n seconds and try again. Timeout after m seconds.
-      res = purrr::safely(
-        function() {
-          # TODO - fix me, oh god it is terrible
-
-          for(i in 1:10) {
-            r = github_api_repo_contributors(repo)
-
-            if (response_status(r) != "202 Accepted") {
-              return(r)
-            }
-
-            Sys.sleep(1)
-          }
-
-          cli_stop("Timeout, no response within 10 seconds.")
-        }
-      )()
+      res = purrr::safely(github_api_repo_contributors)(repo)
 
       status_msg(
         res,
@@ -48,13 +52,26 @@ repo_contributors = function(repo) {
         d = tibble::tibble(
           repo = character(),
           username = character(),
-          commits = integer()
+          commits = integer(),
+          weekly_stats = list()
         )
       } else {
         d = tibble::tibble(
           repo = repo,
           username = purrr::map_chr(contribs, c("author", "login")),
-          commits = purrr::map_int(contribs, "total")
+          commits = purrr::map_int(contribs, "total"),
+          weekly_stats = purrr::map(
+            contribs,
+            function(cont) {
+              dplyr::bind_rows(cont$weeks) %>%
+              dplyr::transmute(
+                week = lubridate::as_datetime(w),
+                additions = a,
+                deletions = d,
+                commits = c
+              )
+            }
+          )
         )
       }
     }
