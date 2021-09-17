ef143087acbb7d0af987fc051eb3d0c35e59677c
Colin Rundel <rundel@gmail.com>
2021-09-17 10:53:17 -0400

    github_orgs

--- R/github_whoami.R
+++ R/github_whoami.R
@@ -27,3 +27,41 @@ github_whoami = function(quiet = FALSE) {
 
   result(res)[["login"]]
 }
+
+
+
+github_api_orgs = function() {
+  ghclass_api_v3_req(
+    endpoint = "GET /user/memberships/orgs"
+  )
+}
+
+#' Returns a tibble with details on the authenticated user's (based on the current PAT)
+#' GitHub organization memberships.
+#'
+#' @param quiet Logical. Should status messages be shown.
+#'
+#' @examples
+#' \dontrun{
+#' github_orgs()
+#' }
+#'
+#' @export
+#'
+github_orgs = function(quiet = FALSE) {
+  res = purrr::safely(github_api_orgs)()
+
+  if (!quiet) {
+    status_msg(
+      res,
+      fail = "Failed to retrieve authenticated user's organization memberships."
+    )
+  }
+
+  res = result(res)
+  tibble::tibble(
+    org = purrr::map_chr(res, c("organization", "login")),
+    role = purrr::map_chr(res, "role"),
+    state = purrr::map_chr(res, "state")
+  )
+}
