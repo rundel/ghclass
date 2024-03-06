7a5b9077906a7bbc9d910b391d4681cef991fad8
Colin Rundel <rundel@gmail.com>
2024-03-06 17:34:13 -0500

    Fix broken filter

--- R/util_filter.R
+++ R/util_filter.R
@@ -1,23 +1,29 @@
-filter_results = function(res, col, pattern = NULL, exclude = FALSE) {
+filter_results = function(res, pattern = NULL, exclude = FALSE, col = NULL) {
   UseMethod("filter_results", res)
 }
 
 #' @exportS3Method
-filter_results.default = function(res, col, pattern = NULL, exclude = FALSE) {
-  if (!is.null(pattern)) {
-    subset = grepl(pattern, res)
-    if (exclude) res = res[!subset]
-    else         res = res[subset]
-  }
-  res
+filter_results.default = function(res, pattern = NULL, exclude = FALSE, col = NULL) {
+  if (is.null(pattern))
+    return(res)
+
+  subset = grepl(pattern, res)
+  if (exclude)
+    res[!subset]
+  else
+    res[subset]
 }
 
 #' @exportS3Method
-filter_results.data.frame = function(res, col, pattern = NULL, exclude = FALSE) {
-  if (!is.null(pattern)) {
-    subset = grepl(pattern, res[[col]])
-    if (exclude) res = res[!subset,]
-    else         res = res[subset,]
-  }
-  res
+filter_results.data.frame = function(res, pattern = NULL, exclude = FALSE, col = NULL) {
+  if (is.null(pattern))
+    return(res)
+
+  stopifnot(!is.null(col))
+
+  subset = grepl(pattern, res[[col]])
+  if (exclude)
+    res[!subset,]
+  else
+    res[subset,]
 }
